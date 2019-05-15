261ccd96151a5aed6523ba141034b16a0c084071
ennaco <ennaco2000@gmail.com>
2019-05-15 16:16:13 +0200

    Creo la clase Reto,con algunas de sus funcionalidades

---
+++ src/primeraprueba/primeraprueba/Reto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeraprueba
+{
+    class Reto
+    {
+        private string nombre;
+        private DateTime fecha_inicial;
+        private DateTime fecha_final;
+        private int idreto;
+        private string descripcion;
+   
+        public string Nombre { get { return nombre; } }
+        public DateTime Fecha_Inicial { get { return fecha_inicial; } }
+        public DateTime Fecha_Final { get { return fecha_final; } }
+        public int IdReto { get { return idreto; } }
+        public string Descripcion { get { return descripcion; }  set { descripcion = value; } }
+
+        public Reto(string nom, DateTime fecha_i, DateTime fecha_f, string desc)
+        {
+            nombre = nom;
+            fecha_inicial = fecha_i;
+            fecha_final = fecha_f;
+            descripcion = desc;
+        }
+
+        public Reto()
+        {
+           
+        }
+        //Funcionalidad para crear el reto
+        public void CrearReto()
+        {
+
+
+
+        }
+
+        //Obtiene una lista de los retos del usuario seleccionado
+        static List<Reto> GetReto(int usuari)
+        {
+
+
+
+        }
+
+
+        //En el caso de que se quiera borrar un reto
+        public void BorrarReto()
+        {
+
+
+        }
+
+
+
+
+
+    }
+}
