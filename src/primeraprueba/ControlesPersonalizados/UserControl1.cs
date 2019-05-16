a2628011a14af368b94dee22ea2577b0dfa8196a
Jose Olmos <olmosjose00@gmail.com>
2019-05-16 19:58:34 +0200

    Empiezo el diseño del control en nuevo proyecto

---
+++ src/primeraprueba/ControlesPersonalizados/UserControl1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlesPersonalizados
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
