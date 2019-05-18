33738750dfc3b27ebd6bbd4e77aff2bc2d3a77e8
joolmo <olmosjose00@gmail.com>
2019-05-18 13:54:25 +0200

    Actualizo control URRv según el diseño

--- src/primeraprueba/ControlesPersonalizados/AccesoURRv.Designer.cs
+++ src/primeraprueba/ControlesPersonalizados/AccesoURRv.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ControlesPersonalizados
 {
-    partial class AccesoURR
+    partial class AccesoURRv
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,39 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.picFoto = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblTitulo
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(82, 177);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblTitulo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(82, 177);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(35, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "label1";
             // 
-            // pictureBox1
+            // picFoto
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(193, 171);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.picFoto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picFoto.Location = new System.Drawing.Point(3, 3);
+            this.picFoto.Name = "picFoto";
+            this.picFoto.Size = new System.Drawing.Size(193, 171);
+            this.picFoto.TabIndex = 1;
+            this.picFoto.TabStop = false;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblTitulo, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.picFoto, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -70,14 +69,14 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(199, 190);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
-            // UserControl1
+            // AccesoURRv
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "UserControl1";
+            this.Name = "AccesoURRv";
             this.Size = new System.Drawing.Size(199, 190);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
@@ -86,8 +85,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox picFoto;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
---
+++ src/primeraprueba/ControlesPersonalizados/AccesoURRv.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlesPersonalizados
+{
+    public partial class AccesoURRv: UserControl
+    {
+
+        #region
+        public Image Foto
+        {
+            get { return picFoto.Image; }
+            set { picFoto.Image = value; }
+        }
+
+        public string Titulo
+        {
+            get { return lblTitulo.Text; }
+            set { lblTitulo.Text = value; }
+        }
+
+        private string tipo;
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        #endregion
+
+        public AccesoURRv()
+        {
+            InitializeComponent();
+        }
+    }
+}
