dc46a07b8714206303a137ed3516da72068ba2c3
joolmo <olmosjose00@gmail.com>
2019-05-18 16:08:35 +0200

    Reparo el control URRh

--- src/primeraprueba/ControlesPersonalizados/AccesoURRh.Designer.cs
+++ src/primeraprueba/ControlesPersonalizados/AccesoURRh.Designer.cs
@@ -43,9 +43,9 @@
             // 
             this.lblInfoBot.AutoSize = true;
             this.lblInfoBot.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblInfoBot.Location = new System.Drawing.Point(3, 100);
+            this.lblInfoBot.Location = new System.Drawing.Point(3, 96);
             this.lblInfoBot.Name = "lblInfoBot";
-            this.lblInfoBot.Size = new System.Drawing.Size(229, 51);
+            this.lblInfoBot.Size = new System.Drawing.Size(122, 50);
             this.lblInfoBot.TabIndex = 0;
             this.lblInfoBot.Text = "label1";
             // 
@@ -53,9 +53,9 @@
             // 
             this.lblInfoMid.AutoSize = true;
             this.lblInfoMid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblInfoMid.Location = new System.Drawing.Point(3, 50);
+            this.lblInfoMid.Location = new System.Drawing.Point(3, 48);
             this.lblInfoMid.Name = "lblInfoMid";
-            this.lblInfoMid.Size = new System.Drawing.Size(229, 50);
+            this.lblInfoMid.Size = new System.Drawing.Size(122, 48);
             this.lblInfoMid.TabIndex = 1;
             this.lblInfoMid.Text = "label2";
             // 
@@ -65,7 +65,7 @@
             this.lblInfoTop.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblInfoTop.Location = new System.Drawing.Point(3, 0);
             this.lblInfoTop.Name = "lblInfoTop";
-            this.lblInfoTop.Size = new System.Drawing.Size(229, 50);
+            this.lblInfoTop.Size = new System.Drawing.Size(122, 48);
             this.lblInfoTop.TabIndex = 2;
             this.lblInfoTop.Text = "label3";
             // 
@@ -74,15 +74,15 @@
             this.picFoto.Dock = System.Windows.Forms.DockStyle.Fill;
             this.picFoto.Location = new System.Drawing.Point(3, 3);
             this.picFoto.Name = "picFoto";
-            this.picFoto.Size = new System.Drawing.Size(1, 151);
+            this.picFoto.Size = new System.Drawing.Size(193, 146);
             this.picFoto.TabIndex = 3;
             this.picFoto.TabStop = false;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.picFoto, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -90,7 +90,7 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(247, 157);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(333, 152);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // tableLayoutPanel2
@@ -101,13 +101,13 @@
             this.tableLayoutPanel2.Controls.Add(this.lblInfoMid, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.lblInfoBot, 0, 2);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(9, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(202, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 3;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(235, 151);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(128, 146);
             this.tableLayoutPanel2.TabIndex = 5;
             // 
             // AccesoURRh
@@ -116,7 +116,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "AccesoURRh";
-            this.Size = new System.Drawing.Size(247, 157);
+            this.Size = new System.Drawing.Size(333, 152);
             ((System.ComponentModel.ISupportInitialize)(this.picFoto)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
