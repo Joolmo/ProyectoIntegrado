0127bee4d96682e62f766233475c325fb1328c4e
ennaco <ennaco2000@gmail.com>
2019-05-20 16:54:04 +0200

    Añado los bucles necesarios para hacer que funcione

--- src/primeraprueba/primeraprueba/Buscador.cs
+++ src/primeraprueba/primeraprueba/Buscador.cs
@@ -10,12 +10,56 @@ namespace primeraprueba
     {
         public List<Receta> BuscarRecetas(string nombre, List<string> tags = null)
         {
-            throw new NotImplementedException();
+            int nletras = nombre.Length;
+            Receta rec;
+            List<Receta> List_Receta = new List<Receta>();
+            for (int i = 0; i < nletras; i++)
+            {
+                string consulta = string.Format("SELECT * FROM recetas WHERE Nombre_Receta like '{0}%';", nombre);
+                List<List<object>> lista = ConexionBBDD.Instanciar().Query(consulta);
+                if (lista.Count > 0)
+                {
+                    foreach (List<object> l1 in lista)
+                    {
+                        rec = new Receta(l1);
+                        List_Receta.Add(rec);
+                    }
+                    break;
+                }
+                else
+                {
+                    nombre = nombre.Substring(0, nombre.Length - 2);
+                }
+
+            }
+            return List_Receta;
         }
 
         public List<Usuario> BuscarUsuarios(string nombre)
         {
-            throw new NotImplementedException();
+            int nletras = nombre.Length;
+            Usuario u;
+            List<Usuario> List_Usuario = new List<Usuario>();
+            for (int i=0; i<nletras; i++)
+            {
+                string consulta = string.Format("SELECT * FROM usuario WHERE Nombre_Usuario like '{0}%';", nombre);
+                List<List<object>> lista = ConexionBBDD.Instanciar().Query(consulta);
+                if (lista.Count > 0)
+                {
+                    foreach (List<object> l1 in lista)
+                    {
+                        u = new Usuario(l1);
+                        List_Usuario.Add(u);
+                    }
+                    break;
+                }
+                else
+                {
+                    nombre = nombre.Substring(0, nombre.Length - 2);
+                }
+                
+            }
+            return List_Usuario;
         }
     }
 }
