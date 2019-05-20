ec650cdb46e09946c41b7199f2f66af65a2dbcc2
joolmo <olmosjose00@gmail.com>
2019-05-20 13:47:46 +0200

    Creo la clase vacia

---
+++ src/primeraprueba/primeraprueba/Buscador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeraprueba
+{
+    class Buscador
+    {
+        public List<Receta> BuscarRecetas(string nombre, List<string> tags = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> BuscarUsuarios(string nombre)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
