7182fb6cadfa962612709efdeef24cc6c57f64a8
ALUMNADO\jaboca <javibc13@gmail.com>
2019-05-23 16:02:16 +0200

    Subido el formulario Crear reto

---
+++ src/primeraprueba/primeraprueba/CrearReto.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace primeraprueba
+{
+	partial class CrearReto
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.lblNombre = new System.Windows.Forms.Label();
+			this.txtNombre = new System.Windows.Forms.TextBox();
+			this.lblDescripción = new System.Windows.Forms.Label();
+			this.ptbApp = new System.Windows.Forms.PictureBox();
+			this.ptbIcon = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.lblFechaini = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+			this.btEnviar = new System.Windows.Forms.Button();
+			this.btCancelar = new System.Windows.Forms.Button();
+			this.tableLayoutPanel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ptbApp)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ptbIcon)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.BackColor = System.Drawing.Color.DodgerBlue;
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.13176F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.86824F));
+			this.tableLayoutPanel2.Controls.Add(this.ptbApp, 1, 0);
+			this.tableLayoutPanel2.Controls.Add(this.ptbIcon, 0, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(1184, 97);
+			this.tableLayoutPanel2.TabIndex = 42;
+			// 
+			// lblNombre
+			// 
+			this.lblNombre.AutoSize = true;
+			this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblNombre.Location = new System.Drawing.Point(91, 116);
+			this.lblNombre.Name = "lblNombre";
+			this.lblNombre.Size = new System.Drawing.Size(69, 20);
+			this.lblNombre.TabIndex = 43;
+			this.lblNombre.Text = "Nombre:";
+			// 
+			// txtNombre
+			// 
+			this.txtNombre.Location = new System.Drawing.Point(166, 118);
+			this.txtNombre.Name = "txtNombre";
+			this.txtNombre.Size = new System.Drawing.Size(128, 20);
+			this.txtNombre.TabIndex = 44;
+			// 
+			// lblDescripción
+			// 
+			this.lblDescripción.AutoSize = true;
+			this.lblDescripción.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblDescripción.Location = new System.Drawing.Point(91, 159);
+			this.lblDescripción.Name = "lblDescripción";
+			this.lblDescripción.Size = new System.Drawing.Size(96, 20);
+			this.lblDescripción.TabIndex = 45;
+			this.lblDescripción.Text = "Descripción:";
+			// 
+			// ptbApp
+			// 
+			this.ptbApp.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ptbApp.Image = global::primeraprueba.Properties.Resources.letra;
+			this.ptbApp.Location = new System.Drawing.Point(194, 3);
+			this.ptbApp.Name = "ptbApp";
+			this.ptbApp.Size = new System.Drawing.Size(987, 91);
+			this.ptbApp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.ptbApp.TabIndex = 25;
+			this.ptbApp.TabStop = false;
+			// 
+			// ptbIcon
+			// 
+			this.ptbIcon.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.ptbIcon.Image = global::primeraprueba.Properties.Resources.logo1;
+			this.ptbIcon.Location = new System.Drawing.Point(45, 3);
+			this.ptbIcon.Name = "ptbIcon";
+			this.ptbIcon.Size = new System.Drawing.Size(101, 87);
+			this.ptbIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ptbIcon.TabIndex = 22;
+			this.ptbIcon.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(95, 182);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(823, 215);
+			this.textBox1.TabIndex = 46;
+			// 
+			// lblFechaini
+			// 
+			this.lblFechaini.AutoSize = true;
+			this.lblFechaini.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblFechaini.Location = new System.Drawing.Point(91, 417);
+			this.lblFechaini.Name = "lblFechaini";
+			this.lblFechaini.Size = new System.Drawing.Size(97, 20);
+			this.lblFechaini.TabIndex = 47;
+			this.lblFechaini.Text = "Fecha inicio:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(91, 453);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(91, 20);
+			this.label2.TabIndex = 48;
+			this.label2.Text = "Fecha final:";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(194, 417);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 49;
+			// 
+			// dateTimePicker2
+			// 
+			this.dateTimePicker2.Location = new System.Drawing.Point(193, 453);
+			this.dateTimePicker2.Name = "dateTimePicker2";
+			this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker2.TabIndex = 50;
+			// 
+			// btEnviar
+			// 
+			this.btEnviar.Location = new System.Drawing.Point(95, 497);
+			this.btEnviar.Name = "btEnviar";
+			this.btEnviar.Size = new System.Drawing.Size(93, 37);
+			this.btEnviar.TabIndex = 51;
+			this.btEnviar.Text = "Enviar";
+			this.btEnviar.UseVisualStyleBackColor = true;
+			// 
+			// btCancelar
+			// 
+			this.btCancelar.Location = new System.Drawing.Point(301, 497);
+			this.btCancelar.Name = "btCancelar";
+			this.btCancelar.Size = new System.Drawing.Size(93, 37);
+			this.btCancelar.TabIndex = 52;
+			this.btCancelar.Text = "Cancelar";
+			this.btCancelar.UseVisualStyleBackColor = true;
+			// 
+			// CrearReto
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1184, 561);
+			this.Controls.Add(this.btCancelar);
+			this.Controls.Add(this.btEnviar);
+			this.Controls.Add(this.dateTimePicker2);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lblFechaini);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.lblDescripción);
+			this.Controls.Add(this.txtNombre);
+			this.Controls.Add(this.lblNombre);
+			this.Controls.Add(this.tableLayoutPanel2);
+			this.Name = "CrearReto";
+			this.Text = "CrearReto";
+			this.tableLayoutPanel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.ptbApp)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ptbIcon)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.PictureBox ptbApp;
+		private System.Windows.Forms.PictureBox ptbIcon;
+		private System.Windows.Forms.Label lblNombre;
+		private System.Windows.Forms.TextBox txtNombre;
+		private System.Windows.Forms.Label lblDescripción;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label lblFechaini;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker2;
+		private System.Windows.Forms.Button btEnviar;
+		private System.Windows.Forms.Button btCancelar;
+	}
+}
---
+++ src/primeraprueba/primeraprueba/CrearReto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primeraprueba
+{
+	public partial class CrearReto : Form
+	{
+		public CrearReto()
+		{
+			InitializeComponent();
+		}
+	}
+}
